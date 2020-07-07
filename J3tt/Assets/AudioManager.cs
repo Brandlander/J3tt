7968936c274609a2ff201f25f1fbb9d6df80e147
Brandon Landeros <brndnlanderos@gmail.com>
2020-07-06 18:58:35 -0700

    Added Audio Capability
    Audio Manager added

--- J3tt/Assets/AudioManager.cs
+++ J3tt/Assets/AudioManager.cs
@@ -1,32 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-    [SerializeField]
-    AudioClip[] clips;
-    [SerializeField]
-    float delayBetween;
+    public Sound[] sounds;
 
-    bool canPlay;
-    AudioSource source;
+    static private AudioManager instance;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        source = GetComponent<AudioSource>();
-        canPlay = true;
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
     }
 
-    public void Play()
+    private void Start()
     {
-        if (!canPlay)
-            return;
+        Play("Full");
+    }
 
-        int clipIndex = Random.Range(0, clips.Length);
-        AudioClip clip = clips[clipIndex];
-        source.PlayOneShot(clip);
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+            return;
+        s.source.Play();
+        s.isPlaying = true;
     }
 }
